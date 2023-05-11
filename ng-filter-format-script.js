4007db98c570bbd20f93f97c1557befc7eed84b6
nirosha <niroshakannan17@gmail.com>
2023-05-11 23:09:35 +0530

    ng filter date example added

--- ng-filter-format-script.js
+++ ng-filter-format-script.js
@@ -3,12 +3,12 @@ var model = angular
 			.controller("ngFilterFormetCtrl", function($scope){
 
 				var employees =[
-				{name:"Sara",dob:"07-07-1996",gender:"Female",salery:"50000"},
-				{name:"samu",dob:"02-02-1995",gender:"Female",salery:"57000"},
-				{name:"Karan",dob:"07-06-1994",gender:"Male",salery:"60000"},
-				{name:"shasha",dob:"07-07-1996",gender:"Female",salery:"55000"},
-				{name:"srither",dob:"17-10-1995",gender:"Male",salery:"80000"},
-				{name:"jotha",dob:"04-11-1996",gender:"Female",salery:"50000"}
+				{name:"Sara",dob:new Date("07-07-1996"),gender:"Female",salery:"50000"},
+				{name:"samu",dob:new Date("02-02-1995"),gender:"Female",salery:"57000"},
+				{name:"Karan",dob:new Date("07-06-1994"),gender:"Male",salery:"60000"},
+				{name:"shasha",dob:new Date("07-07-1996"),gender:"Female",salery:"55000"},
+				{name:"srither",dob:new Date("12-10-1995"),gender:"Male",salery:"80000"},
+				{name:"jotha",dob:new Date("04-11-1996"),gender:"Female",salery:"50000"}
 				];
 
 				$scope.emp = employees;
